80b88150b5118192cb0743a932f5180d6f85671f
Panasik Vladislav <vladpanasik674@gmail.com>
2024-11-23 17:05:04 +0300

    fixing bugs with first lab

--- app/src/main/java/com/example/helloapp/MainActivity.kt
+++ app/src/main/java/com/example/helloapp/MainActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -19,10 +22,13 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.FileProvider
@@ -32,6 +38,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.helloapp.ui.theme.HelloAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,9 +55,9 @@ class ComposeFileProvider : FileProvider(R.xml.files_paths)
 @Composable
 fun Main() {
     val navController = rememberNavController()
-    var name by remember { mutableStateOf("") }
+    var name by rememberSaveable{ mutableStateOf("") }
 
-    Column(Modifier.padding(8.dp)) {
+    Column() {
         NavHost(navController, startDestination = NavRoutes.Home.route, modifier = Modifier.weight(1f)) {
             composable(NavRoutes.Home.route) {
                 Home(name = name, onNameChange = { name = it })
@@ -133,7 +140,7 @@ val trainList = listOf(
 
 @Composable
 fun TrainListScreen() {
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+    Column(modifier = Modifier.fillMaxSize().padding(32.dp)) {
         Text(
             text = "Пункт назначения",
             fontSize = 24.sp,
@@ -158,49 +165,40 @@ fun TrainItem(train: Train) {
 }
 
 @Composable
-fun Home(
-    name: String,
-    onNameChange: (String) -> Unit,
-    modifier: Modifier = Modifier
-) {
+fun Home(name: String, onNameChange: (String) -> Unit, modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize().background(Color.Blue),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        var text = "Канаш Алексей Васильевич"
+        var text by remember { mutableStateOf(name) }
 
         Text(
-            text = name,
-            modifier = modifier,
-            fontSize = 24.sp,
-            color = Color.Blue
+            text = "Hello $name!",
+            modifier = modifier.padding(16.dp),
+            fontSize = 48.sp
         )
 
-        Row(
-            modifier = Modifier,
-            horizontalArrangement = Arrangement.spacedBy(2.dp)
-        ) {
-            Button(
-                onClick = { onNameChange(text) },
-                colors = ButtonDefaults.buttonColors(
-                    Color(0xFF3CBD18)
-                )
-            ) {
-                Text(text = "Вывести имя", fontSize = 24.sp, color = Color.Green)
-            }
-            Button(
-                onClick = { onNameChange("") },
-                colors = ButtonDefaults.buttonColors(
-                    Color(0xFF3CBD18)
-                )
-            ) {
-                Text(text = "X", fontSize = 24.sp, color = Color.Green)
-            }
+        Image(
+            painter = painterResource(id = R.drawable.students),
+            contentDescription = "Hello Student Image",
+            modifier = Modifier
+                    .size(130.dp)
+        )
+
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text(text = "Введите имя") },
+            placeholder = { Text(text = "Student") },
+            modifier = modifier.padding(4.dp)
+        )
+
+        Button(onClick = { onNameChange(text) }) {
+            Text(text = "Приветствовать")
         }
     }
 }
-
 
 
 data class BarItem(
